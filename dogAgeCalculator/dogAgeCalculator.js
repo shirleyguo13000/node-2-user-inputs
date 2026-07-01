ee0c8e1cbb74770b1da886bfbce46534c2eb7afa
shirleyguo13000 <shirleyguo13000@gmail.com>
2026-07-01 12:58:43 -0400

    Update dogAgeCalculator.js

--- dogAgeCalculator/dogAgeCalculator.js
+++ dogAgeCalculator/dogAgeCalculator.js
@@ -10,6 +10,7 @@ node dogAgeCalculator.js
 */
 // declaring dynamic input so user can enter their dog's name and human year
 let dogName = process.argv[2];
+// Number is added to ensure that the data type remains a number as opposed to a string
 let humanYear = Number(process.argv[3]);
 
 // conditional for if their dog is 1 years old, 2 years old or anything above 2.
@@ -22,6 +23,8 @@ if (humanYear === 1) {
     `Your dog, ${dogName}, is 2 years old, but that's 24 years old in dog years!`,
   );
 } else {
+  // declaring new variable to store the calculated dog year from users' input
+  // calculations are based on how many human years the dog has been alive
   let dogCalculation = 24 + 5 * (humanYear - 2);
   console.log(
     `Your dog, ${dogName}, is ${humanYear} years old, but that's ${dogCalculation} years old in dog years!`,
